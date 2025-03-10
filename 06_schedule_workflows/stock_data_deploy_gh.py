1d9fe04c9d8a18a3390f22c3569b0939186e4060
Jeff Hale <jeffmshale@gmail.com>
2025-03-10 16:45:58 -0400

    update

--- 06_schedule_workflows/stock_data_deploy_gh.py
+++ 06_schedule_workflows/stock_data_deploy_gh.py
@@ -4,7 +4,7 @@ from prefect import flow
 if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/PrefectHQ/write-workflows-course.git",
-        entrypoint="03_start_observing/stock_data_flow.py:fetch_and_save_stock_data",
+        entrypoint="03_start_observing/stock_data_flow.py:stock_data_flow",
     ).serve(
         name="stock-data-from-gh-repo",
         cron="0 0 0 * *",
